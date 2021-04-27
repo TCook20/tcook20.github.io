b5044226451e16f44ddf3af46b0c036ee8408f6a
Travis Cook <twcook2007@gmail.com>
2021-04-27 16:05:20 -0400

    Add routing

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Home from './components/Home';
 import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <article>
-          <Projects />
-        </article>
-        <Sidebar />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <main>
+          <article>
+            <Route exact path="/" component={ () => <Home />} />
+            <Route path="/projects" component={ () => <Projects />} />
+          </article>
+          <Sidebar />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
