10b21c4e7d8526552d5c6a219998e8e509aea16a
Travis Cook <twcook2007@gmail.com>
2021-07-27 21:06:54 -0400

    Placeholder

--- portfolio-app/src/components/Home.js
+++ portfolio-app/src/components/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <p>Coming soon...</p>
+      <p>Trained counselor turned front-end web developer with several years of experience in higher education. Versed in WordPress development. Experienced in PHP, HTML, CSS/SCSS, JavaScript, and ReactJS. Excited by exploring cutting edge web technology that improves efficiency and provides a clean, yet enjoyable experience for users. Driven by the desire to improve efficiency.</p>
     );
   }
 }
