a5f82437a8021e64d6eb6d9aedaa5c41bfe5c779
Travis Cook <twcook2007@gmail.com>
2021-07-27 21:07:29 -0400

    Add url and description

--- portfolio-app/src/components/Project.js
+++ portfolio-app/src/components/Project.js
@@ -3,19 +3,23 @@ import PropTypes from 'prop-types';
 
 class Project extends PureComponent {
   static propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    url: PropTypes.string,
+    description: PropTypes.string,
   };
 
   render() {
     const {
-      title
+      title,
+      url,
+      description
     } = this.props;
 
     return (
       <div className="project-card">
-        <h3>{ title }</h3>
+        <h3><a href={ url }>{ title }</a></h3>
         <div class="card-body">
-
+          { description }
         </div>
       </div>
     );
