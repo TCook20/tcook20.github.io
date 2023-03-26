6397731c2525fb258aeb6fd4da8d491f16b79570
Travis <twcook2007@gmail.com>
2023-03-26 17:27:55 -0400

    Import style

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import './style.scss';
+
 export interface ProjectProps {
   title: string
   url: string
@@ -10,10 +12,10 @@ const Project = ({ title, url, description }: ProjectProps) => {
 
   return (
     <div className={ classnames( 'project-card' ) }>
-      <h3 className={ classnames( 'project-title' ) }>
+      <h3 className={ classnames( 'project-card__title' ) }>
         <a href={ url }>{ title }</a>
       </h3>
-      <div className={ classnames( 'project-card-body' ) }>
+      <div className={ classnames( 'project-card__description' ) }>
         { description }
       </div>
     </div>
